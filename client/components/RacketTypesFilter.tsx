f0446d1c22f693dc62cdee753f5943bec3bc74b1
sbayce <youssef2311@gmail.com>
2024-10-30 21:17:16 +0300

    filter checkboxes

---
+++ client/components/RacketTypesFilter.tsx
@@ -0,0 +1,62 @@
+"use client"
+import { racketTypes } from "@/app/page"
+import { Checkbox } from "@/components/ui/checkbox"
+import { useRouter, useSearchParams, usePathname } from "next/navigation"
+import { useState, useEffect } from "react"
+
+const RacketTypesFilter = () => {
+
+    const searchParams = useSearchParams();
+    const pathName = usePathname();
+    const { replace } = useRouter();
+    const initialCheckedBoxes = searchParams.getAll('type');
+    const [checkedBoxes, setCheckedBoxes] = useState<string[]>(initialCheckedBoxes);
+
+    useEffect(() => {
+        setCheckedBoxes(searchParams.getAll('type'));
+    }, [searchParams]);
+
+    function handleCheck(racketType: string) {
+        const params = new URLSearchParams(searchParams);
+
+        // Toggle the racket type parameter
+        if (params.has('type')) {
+            const types = params.getAll('type');
+            if (types.includes(racketType)) {
+                types.splice(types.indexOf(racketType), 1);
+            } else {
+                types.push(racketType);
+            }
+            params.delete('type');
+            types.forEach(type => params.append('type', type));
+        } else {
+            params.append('type', racketType);
+        }
+        replace(`${pathName}?${params.toString()}`);
+    }
+  return (
+    <>
+        <h3 className="font-semibold mb-2">Racket Types</h3>
+        <div className="flex flex-col gap-2 mb-4">
+            {racketTypes.map(racketType => (
+                <div key={racketType} className="flex items-center justify-between">
+                    <label
+                        htmlFor={racketType}
+                        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    >
+                        {racketType}
+                    </label>
+                    <Checkbox
+                        checked={checkedBoxes.includes(racketType)}
+                        onCheckedChange={() => handleCheck(racketType)}
+                        className="w-6 h-6"
+                        id={racketType}
+                    />
+                </div>
+            ))}
+        </div>
+    </>
+  )
+}
+
+export default RacketTypesFilter
--- client/components/SideBar.tsx
+++ client/components/SideBar.tsx
@@ -1,28 +1,13 @@
-import { racketTypes, racketBrands } from "@/app/page"
-import Link from "next/link"
-import { Checkbox } from "@/components/ui/checkbox"
 import BrandList from "./BrandList"
+import RacketTypesFilter from "./RacketTypesFilter"
 
 const SideBar = () => {
-  return (
-    <div className="p-4">
-        <h3 className="font-semibold mb-2">Racket Types</h3>
-        <div className="flex flex-col gap-2 mb-4">
-            {racketTypes.map(racketType => 
-            <div key={racketType} className="flex items-center justify-between">
-                <label
-                    htmlFor={racketType}
-                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                >
-                    {racketType}
-                </label>
-                <Checkbox className="w-6 h-6" id={racketType} />
-            </div>)}
+    return (
+        <div className="p-4">
+            <RacketTypesFilter />
+            <BrandList />
         </div>
-        <BrandList />
-        
-    </div>
-  )
+    )
 }
 
-export default SideBar
+export default SideBar;
