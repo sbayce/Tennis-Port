c3fc5022b469373e0b5be37b6971b1c13f5ec24a
sbayce <youssef2311@gmail.com>
2024-10-30 21:16:45 +0300

    styles

--- client/components/ProductsGrid.tsx
+++ client/components/ProductsGrid.tsx
@@ -1,3 +1,6 @@
+import DiameterIcon from '../icons/diameter-head.svg'
+import StringsIcon from '../icons/racket-strings.svg'
+import WeightIcon from '../icons/weight.svg'
 
 type ProductsGridProps = {
     products: any[]
@@ -5,20 +8,29 @@ type ProductsGridProps = {
 
 const ProductsGrid = ({ products }: ProductsGridProps) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-2 w-full">
         {products.map(product =>
-            <div key={product.name} className="border rounded-sm p-6 flex">
-                <img className="w-24 md:w-32 lg:w-44 h-24 md:h-32 lg:h-44" src={product.image} alt="product-img" />
-                <div className="flex flex-col gap-4">
+            <div key={product.name} className="border rounded-sm 2xl:p-6 flex flex-col xl:flex-row gap-4 items-center">
+                <img className="w-24 2xl:w-32 3xl:w-44 h-24 2xl:h-32 3xl:h-44" src={product.image} alt="product-img" />
+                <div className="flex flex-col gap-4 w-full items-center xl:items-start">
                     <img className="w-14" src={product.brand} alt="product-brand" />
-                    <p>{product.type}</p>
-                    <p>{product.name}</p>
-                    <p>{product.price}</p>
+                    <p className="text-slate-500 text-sm font-bold">{product.name}</p>
+                    <p className="text-sm font-semibold">{product.type}</p>
+                    <p className="text-lg font-bold text-gray-700">{product.price} <span className="text-sm">EGP</span></p>
                     <p>{product.rating}</p>
-                    <div className="flex gap-2">
-                        <p>{product.width}</p>
-                        <p>{product.height}</p>
-                        <p>{product.weight}</p>
+                    <div className="flex gap-2 items-center text-sm text-gray-500">
+                        <div className='flex flex-col items-center'>
+                            <DiameterIcon className='w-8 h-8' />
+                            <p>{product.width}</p>
+                        </div>
+                        <div className='flex flex-col items-center'>
+                            <StringsIcon className='w-8 h-8' />
+                            <p>{product.height}</p>
+                        </div>
+                        <div className='flex flex-col items-center'>
+                            <WeightIcon className='w-8 h-8' />
+                            <p>{product.weight}</p>
+                        </div>
                     </div>
                 </div>
             </div>
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -66,7 +66,15 @@ const config: Config = {
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		}
-  	}
+  	},
+	screens: {
+		'sm': '640px',
+		'md': '768px',
+		'lg': '1024px',
+		'xl': '1280px',
+		'2xl': '1480px',
+		'3xl': '1618px',
+	}
   },
   plugins: [require("tailwindcss-animate")],
 };
