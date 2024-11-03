7aa622b561640235e421c0051b5c6ce373e21d78
sbayce <youssef2311@gmail.com>
2024-11-03 20:48:03 +0200

    image hover effect

--- client/components/ProductsGrid.tsx
+++ client/components/ProductsGrid.tsx
@@ -4,7 +4,9 @@ import StringsIcon from '@/icons/racket-strings.svg'
 import WeightIcon from '@/icons/weight.svg'
 import { useCart } from '@/contexts/CartContext'
 import Item from '@/contexts/types/item'
-import BabolatIcon from '@/icons/wilson.svg'
+import WilsonRedIcon from '@/icons/wilson-red.svg'
+import { useState } from 'react'
+import { motion } from 'framer-motion'
 
 type ProductsGridProps = {
     products: any[]
@@ -12,23 +14,34 @@ type ProductsGridProps = {
 
 const ProductsGrid = ({ products }: ProductsGridProps) => {
     const cartCtx = useCart()
+    const [isHover, setIsHover] = useState<string | null>(null)
     function handleAdd(product: Item) {
         cartCtx?.addItem(product)
     }
     console.log(cartCtx?.items)
+    console.log("hover: ", isHover)
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-2 w-full">
+    <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-20 w-full shrink">
         {products.map(product =>
-            <div key={product.name} className="border rounded-sm 2xl:p-6 flex flex-col xl:flex-col gap-4 items-center">
-                <img className="w-24 2xl:w-80 h-24 2xl:h-80" src={product.image} alt="product-img" />
-                <div className="flex flex-col gap-4 w-full items-center xl:items-center">
-                    <BabolatIcon className="w-20" />
-                    {/* <img className="w-14" src={product.brand} alt="product-brand" /> */}
-                    <p className="text-zinc-900 text-xl">{product.name}</p>
+            <div key={product.name} className="flex flex-col xl:flex-col gap-4 items-center cursor-pointer">
+                <div className='relative w-full h-full overflow-hidden' onMouseOver={() => setIsHover(product.name)} onMouseOut={() => setIsHover(null)}>
+                    <motion.img initial={{opacity: 1}} animate={{ opacity: isHover === product.name ? 0 : 1 }} transition={{duration: 0.1}} className={`w-full h-full max-h-[100%] max-w-[100%] object-cover`}
+                            src={product.image}
+                            alt="product-img"
+                    />
+                    <motion.img initial={{opacity: 0}} animate={{ opacity: isHover === product.name ? 1 : 0 }} transition={{duration: 0.1}} className={`w-full h-full max-h-[100%] max-w-[100%] object-cover absolute top-0`}
+                            src={product.image2}
+                            alt="product-img"
+                    />
+                </div>
+                {/* <img className="w-full h-full max-h-[100%] max-w-[100%] object-cover" src={isHover === product.name? imgUrl: product.image} alt="product-img" /> */}
+                <div className="flex flex-col gap-0 w-full items-center xl:items-center relative">
+                    {product.brand}
+                    {/* <WilsonRedIcon className="w-20 h-20" /> */}
+                    <p className="text-zinc-900 text-md font-semibold">{product.name}</p>
                     <p className="text-sm font-semibold">{product.type}</p>
                     <p className="text-lg font-bold text-gray-700">{product.price} <span className="text-sm">EGP</span></p>
-                    {/* <p>{product.rating}</p> */}
-                    <div className="flex gap-2 items-center text-sm text-gray-500">
+                    <div className="flex items-center text-xs text-gray-500 absolute right-0 -top-16">
                         <div className='flex flex-col items-center'>
                             <DiameterIcon className='w-8 h-8' />
                             <p>{product.width}</p>
