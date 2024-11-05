e95b9b3e1c3e1c5fb82e3ee0c9d786a13b3fcc14
sbayce <youssef2311@gmail.com>
2024-11-05 19:14:17 +0200

    search params filtering

--- client/components/ActiveFilters.tsx
+++ client/components/ActiveFilters.tsx
@@ -3,32 +3,28 @@ import { useSearchParams, useRouter, usePathname } from 'next/navigation'
 
 const ActiveFilters = () => {
     const searchParams = useSearchParams()
-    const brands = searchParams.getAll('brand')
-    const racketTypes = searchParams.getAll('type')
     const filters = Array.from(searchParams.values())
     console.log("filters:", filters)
     const { replace } = useRouter()
     const pathName = usePathname();
 
     function removeFilter(filter: string) {
+        console.log("filter: ", filter)
         const params = new URLSearchParams(searchParams.toString())
-        const brands = params.getAll('brand')
-        const types = params.getAll('type')
-
-        if (brands.includes(filter)) {
-            params.delete('brand')
-            brands.filter(b => b !== filter).forEach(b => params.append('brand', b))
-        }else if (types.includes(filter)) {
-            params.delete('type')
-            types.filter(t => t !== filter).forEach(t => params.append('type', t))
-        }
+        params.entries().forEach(p => {
+            if(p[1] === filter) {
+                const paramValues = params.getAll(p[0])
+                params.delete(p[0])
+                paramValues.filter(val => val !== filter).forEach(val => params.append(p[0], val))
+            }
+        })
         replace(`${pathName}?${params.toString()}`, { scroll: false })
     }
 
     return filters.length > 0 ? (
-        <div className='flex gap-2 text-sm text-gray-500'>
+        <div className='flex gap-2 text-sm text-black px-4'>
             {filters.map(filter => (
-                <div key={filter} className='border p-2 flex items-center'>
+                <div key={filter} className='bg-[#41414134] p-3 rounded-3xl flex items-center'>
                     {filter}
                     <button onClick={() => removeFilter(filter)} className='ml-2'>
                         X
--- client/components/PriceFilter.tsx
+++ client/components/PriceFilter.tsx
@@ -1,24 +1,32 @@
 import { useState, useRef } from "react"
 import { DualRangeSlider } from "./ui/dual-range-slider"
+import FadeInMotionDiv from "./framer/FadeInMotionDiv"
+import { usePathname, useSearchParams, useRouter } from "next/navigation"
 
 const MIN_PRICE = 0
 const MAX_PRICE = 20000
 
 const PriceFilter = () => {
-  const [priceRange, setPriceRange] = useState<number[]>([MIN_PRICE, MAX_PRICE])
+  const path = usePathname()
+  const searchParams = useSearchParams()
+  const { replace } = useRouter()
+  const minQuery = Number(searchParams.get("price.min")) || MIN_PRICE
+  const maxQuery = Number(searchParams.get("price.max")) || MAX_PRICE
+  const [priceRange, setPriceRange] = useState<number[]>([minQuery, maxQuery])
   let previousMin = MIN_PRICE
   let previousMax = MAX_PRICE
   const minInputRef = useRef<HTMLInputElement>(null)
   const maxInputRef = useRef<HTMLInputElement>(null)
+  
 
   // function to update prince range based on which filed was submitted (min or max)
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>, type: 'min' | 'max') {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>, type: 'min' | 'max') => {
     let value = type === 'max' && e.target.value === ''? MAX_PRICE : Number(e.target.value) // if no value entered at max -> defaults to MAX_PRICE
     
     // skip if values didn't change
-    if( (type === 'min' && previousMin === value) || (type === 'max' && previousMax === value) ){
-      return
-    }
+    // if( (type === 'min' && previousMin === value) || (type === 'max' && previousMax === value) ){
+    //   return
+    // }
 
     if(type === 'min' && value >= priceRange[1]){
       value = priceRange[1] - 1
@@ -36,6 +44,18 @@ const PriceFilter = () => {
       updatedPrice[index] = value
       return updatedPrice
     })
+    const params = new URLSearchParams(searchParams.toString())
+    if(type === 'min' && value === MIN_PRICE) {
+      params.delete('price.min')
+      return replace(`${path}?${params.toString()}`, {scroll: false})
+    }
+    if(type === 'max' && value === MAX_PRICE) {
+      params.delete('price.max')
+      return replace(`${path}?${params.toString()}`, {scroll: false})
+    }
+    params.set(`price.${type}`, String(value))
+    replace(`${path}?${params.toString()}`, {scroll: false})
+    console.log(params.toString())
   }
   function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>, ref: React.RefObject<HTMLInputElement>) {
     if (e.key === "Enter") ref.current?.blur()
@@ -52,9 +72,7 @@ function handleOnChange(e: React.ChangeEvent<HTMLInputElement>, type: 'min' | 'm
 
   console.log(priceRange)
   return (
-    <div>
-      <h3 className="font-semibold mb-2">Price</h3>
-      <div className='flex flex-col gap-4 items-center mt-4'>
+      <FadeInMotionDiv className='flex flex-col gap-4 items-center px-2 mt-4'>
           <DualRangeSlider
             value={priceRange}
             onValueChange={setPriceRange}
@@ -64,22 +82,19 @@ function handleOnChange(e: React.ChangeEvent<HTMLInputElement>, type: 'min' | 'm
             step={1}
           />
           <div className="flex gap-2">
-
               <input ref={minInputRef} value={priceRange[0] !== MIN_PRICE ? priceRange[0] : ""} 
                 onChange={(e) => handleOnChange(e, "min")} placeholder={String(MIN_PRICE)} 
                 onKeyDown={(e) => handleKeyDown(e, minInputRef)} onBlur={(e) => handleChange(e, "min")} 
-                min={MIN_PRICE} max={MAX_PRICE} className="border rounded-xl py-3 px-2 w-24 text-sm text-center" 
+                min={MIN_PRICE} max={MAX_PRICE} className="border rounded-xl py-3 px-2 w-20 text-sm text-center" 
                 type="number" name="min-price" />
 
               <input ref={maxInputRef} value={priceRange[1] !== MAX_PRICE ? priceRange[1] : ""} 
                 onChange={(e) => handleOnChange(e, "max")} placeholder={String(MAX_PRICE)} 
                 onKeyDown={(e) => handleKeyDown(e, maxInputRef)} onBlur={(e) => handleChange(e, "max")} 
-                min={MIN_PRICE} max={MAX_PRICE} className="border rounded-xl py-3 px-2 w-24 text-sm text-center" 
+                min={MIN_PRICE} max={MAX_PRICE} className="border rounded-xl py-3 px-2 w-20 text-sm text-center" 
                 type="number" name="max-price" />
-
           </div>
-      </div>
-    </div>
+      </FadeInMotionDiv>
   )
 }
 
