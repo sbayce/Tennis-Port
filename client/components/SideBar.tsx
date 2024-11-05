549857b39f90bca87a2f19c77fe392ab1b35a56e
sbayce <youssef2311@gmail.com>
2024-11-05 19:12:17 +0200

    add accordion to sidebar

--- client/components/SideBar.tsx
+++ client/components/SideBar.tsx
@@ -1,15 +1,57 @@
-import BrandsFilter from "./BrandsFilter";
+import BrandsFilter from "./BrandsFilter"
 import RacketTypesFilter from "./RacketTypesFilter"
-import PriceFilter from "./PriceFilter";
+import WeightsFilter from "./WeightsFilter"
+import PriceFilter from "./PriceFilter"
+import { useSearchParams } from "next/navigation"
+import { AnimatePresence } from "framer-motion"
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+  } from "@/components/ui/accordion"
 
 const SideBar = () => {
+    const searchParams = useSearchParams()
+    // extract the required search params ['type', 'brand', 'price'] without duplicates
+    const allParams = Array.from(new Set(Array.from(searchParams.keys()).reduce((res: string[], k) => {
+        const required = ['type', 'brand', 'price']
+        const param = k.split(".")[0]
+        required.includes(param) && res.push(param) // split price params from 'price.max/price.min' to 'price'
+        return res
+    }, [])))
     return (
-        <div className="p-4 sticky h-screen top-24 flex flex-col gap-4">
-            <RacketTypesFilter />
-            <BrandsFilter />
-            <PriceFilter />
-        </div>
+        <AnimatePresence mode="popLayout">
+            <div className="p-4 sticky h-screen top-24 flex flex-col gap-4">
+                <Accordion defaultValue={allParams} type="multiple" className="w-48">
+                    <AccordionItem data-sta value="type">
+                        <AccordionTrigger>Racket Types</AccordionTrigger>
+                        <AccordionContent>
+                            <RacketTypesFilter />
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="brand">
+                        <AccordionTrigger>Racket Brands</AccordionTrigger>
+                        <AccordionContent>
+                            <BrandsFilter />
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="price">
+                        <AccordionTrigger>Price</AccordionTrigger>
+                        <AccordionContent>
+                            <PriceFilter />
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="weight">
+                        <AccordionTrigger>Weight</AccordionTrigger>
+                        <AccordionContent>
+                            <WeightsFilter />
+                        </AccordionContent>
+                    </AccordionItem>
+                </Accordion>
+            </div>
+        </AnimatePresence>
     )
 }
 
-export default SideBar;
+export default SideBar
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -65,16 +65,38 @@ const config: Config = {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
   	},
-	screens: {
-		'sm': '640px',
-		'md': '700px',
-		'lg': '1000px',
-		'xl': '1280px',
-		'2xl': '1400px',
-		'3xl': '1618px',
-	}
+  	screens: {
+  		sm: '640px',
+  		md: '700px',
+  		lg: '1000px',
+  		xl: '1280px',
+  		'2xl': '1400px',
+  		'3xl': '1618px'
+  	}
   },
   plugins: [require("tailwindcss-animate")],
 };
