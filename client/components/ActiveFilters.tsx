b4dee5f19d9664ed4b69ddfb7caab24ff3433489
sbayce <youssef2311@gmail.com>
2024-11-06 02:38:39 +0200

    added clear filters option

--- client/components/ActiveFilters.tsx
+++ client/components/ActiveFilters.tsx
@@ -1,12 +1,13 @@
 "use client"
 import { useSearchParams, useRouter, usePathname } from 'next/navigation'
+import Link from 'next/link'
 
 const ActiveFilters = () => {
     const searchParams = useSearchParams()
     const filters = Array.from(searchParams.values())
     console.log("filters:", filters)
     const { replace } = useRouter()
-    const pathName = usePathname();
+    const path = usePathname();
 
     function removeFilter(filter: string) {
         console.log("filter: ", filter)
@@ -18,11 +19,11 @@ const ActiveFilters = () => {
                 paramValues.filter(val => val !== filter).forEach(val => params.append(p[0], val))
             }
         })
-        replace(`${pathName}?${params.toString()}`, { scroll: false })
+        replace(`${path}?${params.toString()}`, { scroll: false })
     }
 
     return filters.length > 0 ? (
-        <div className='flex gap-2 text-sm text-black px-4'>
+        <div className='flex flex-wrap gap-2 items-center text-sm text-black px-4'>
             {filters.map(filter => (
                 <div key={filter} className='bg-[#41414134] p-3 rounded-3xl flex items-center'>
                     {filter}
@@ -31,6 +32,9 @@ const ActiveFilters = () => {
                     </button>
                 </div>
             ))}
+            { filters.length >= 2 && <Link className={`relative after:transition-all after:duration-300 ml-2 after:content-[''] after:absolute 
+                after:left-0 after:bottom-0 after:w-0 after:h-[1px] after:bg-[#202223] hover:after:w-full`} 
+                href={`${path}`} scroll={false}>Clear all</Link> }
         </div>
     ) : null
 }
