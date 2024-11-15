8c2cb7d29095d40047686c96382e5931b52cb98c
sbayce <youssef2311@gmail.com>
2024-11-15 16:54:06 +0200

    search animation

--- client/components/SearchMenu.tsx
+++ client/components/SearchMenu.tsx
@@ -10,7 +10,9 @@ import {
   import debounce from 'lodash.debounce'
   import trpc from "@/trpcClient"
   import { useCallback, useState } from "react"
-import { motion } from "framer-motion"
+import { motion, AnimatePresence } from "framer-motion"
+import { Search } from "lucide-react"
+import Link from "next/link"
   
   type SearchResult = {
       id: string,
@@ -20,6 +22,44 @@ import { motion } from "framer-motion"
       price: number
   }
   
+  const ProductsVariant = {
+    hidden: {
+        opacity: 0,
+        height: 0,
+    },
+    visible: {
+        opacity: 1,
+        height: 700,
+        transition: {
+            when: "beforeChildren",
+        }
+    },
+    exit: {
+        opacity: 0,
+        height: 0,
+        transition: {
+            when: "afterChildren",
+        }
+    }
+  }
+  const ProductVariant = {
+    hidden: {
+        opacity: 0,
+    },
+    visible: {
+        opacity: 1,
+        transition: {
+            delay: 0.1
+        }
+    },
+    exit: {
+        opacity: 0,
+        transition: {
+            duration: 0.1
+        }
+    }
+  }
+
 const SearchMenu = () => {
     const [searchResults, setSearchResults] = useState<SearchResult[]>([])
     const [searchInput, setSearchInput] = useState("")
@@ -31,6 +71,12 @@ const SearchMenu = () => {
     const debouncedSearch = debounce((searchInput: string) => searchProducts(searchInput), 1000)
 
     const handleChange = useCallback((value: string) => {
+        if(value === ""){
+            debouncedSearch.cancel() // cancel the ongoing debounce (if exists)
+            setSearchResults([])
+            setSearchInput(value)
+            return
+        }
         setSearchInput(value)
         debouncedSearch(value)
     }, [])
@@ -41,29 +87,33 @@ const SearchMenu = () => {
     console.log("search results: ", searchResults)
   return (
         <Sheet>
-            <SheetTrigger>Search</SheetTrigger>
+            <SheetTrigger><Search /></SheetTrigger>
             <SheetHeader>
                 <SheetTitle />
                 <SheetDescription />
             </SheetHeader>
             <SheetContent className="pb-0" side="top">
                 <div className="mx-10 mb-10">
-                    <input value={searchInput} onChange={(e) => handleChange(e.target.value)} className={`w-full text-2xl p-2 border-b-2 border-b-transparent focus:outline-none
+                    <input value={searchInput} onChange={(e) => handleChange(e.target.value)} className={`w-full text-2xl p-2 border-b-2 border-opacity-30 focus:outline-none
                         focus:border-b-[#202223] transition-colors duration-300`} type="text" placeholder="Search for..." />
                 </div>
-                {searchResults.length > 0 && 
-                <motion.div initial={{opacity: 0, height: 0}} animate={{opacity: 1, height: 700}} transition={{duration: 0.3}}>
-                <ScrollArea className="h-[700px]">
-                    {searchResults.map(product => <div key={product.id} className="flex gap-2 mt-4">
-                        <img className="w-32 h-32" src={product.image} />
-                        <div className="flex flex-col gap-2">
-                            <p>{product.name}</p>
-                            <p>{product.brand}</p>
-                            <p>{product.price}</p>
-                        </div>
-                    </div>)}
-                </ScrollArea>
-                </motion.div>}
+                <AnimatePresence>
+                    {searchResults.length > 0 && 
+                        <motion.div key="products-list" variants={ProductsVariant} initial="hidden" animate="visible" exit="exit">
+                            <ScrollArea className="h-[700px]">
+                                {searchResults.map(product => <Link key={product.id} href={`/product/${product.id}`}>
+                                <motion.div variants={ProductVariant} className="flex flex-col xs:flex-row gap-2 mt-4 items-center">
+                                        <img className="w-32 h-32" src={product.image} />
+                                        <div className="flex flex-col gap-1">
+                                            <p className="font-semibold">{product.name}</p>
+                                            <p className="text-xs">{product.brand}</p>
+                                            <p className="text-sm">{product.price} EGP</p>
+                                        </div>
+                                </motion.div>
+                                </Link>)}
+                            </ScrollArea>
+                    </motion.div>}
+                </AnimatePresence>
             </SheetContent>
         </Sheet>
 
