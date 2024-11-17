98f627ffa0238d6aa45d25d081213dfc85eb9926
Turbobot <turbobot@vercel.com>
2024-11-17 17:57:48 +0200

    feat(create-turbo): apply official-starter transform

--- client/trpcClient.ts
+++
@@ -1,9 +0,0 @@
-import { createTRPCProxyClient, httpBatchLink } from '@trpc/client'
-import { AppRouter } from '../server/index'
-
-const trpc = createTRPCProxyClient<AppRouter>({
-    links: [httpBatchLink({
-        url: String(process.env.NEXT_PUBLIC_BACKEND_URL)
-    })]
-})
-export default trpc
--- server/routers/checkout.ts
+++
@@ -1,8 +0,0 @@
-import { t } from "../trpc";
-import checkoutProcedure from "../mutations/checkout";
-
-const checkoutRouter = t.router({
-    checkout: checkoutProcedure
-})
-
-export default checkoutRouter
