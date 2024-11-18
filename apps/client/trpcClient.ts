e97191baa7ef210d3ca024aaf7f7dd3387e35f43
sbayce <youssef2311@gmail.com>
2024-11-18 15:29:47 +0200

    middleware parsers

--- apps/client/trpcClient.ts
+++ apps/client/trpcClient.ts
@@ -3,7 +3,13 @@ import { AppRouter } from 'trpc/routers/index'
 
 const trpc = createTRPCProxyClient<AppRouter>({
     links: [httpBatchLink({
-        url: String(`${process.env.NEXT_PUBLIC_BACKEND_URL}/trpc`)
+        url: String(`${process.env.NEXT_PUBLIC_BACKEND_URL}/trpc`),
+        fetch(url, options) {
+            return fetch(url, {
+                ...options,
+                credentials: 'include'
+            })
+        }
     })]
 })
 export default trpc
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import cors from 'cors'
+import * as bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
 import { createExpressMiddleware } from 'trpc/middlewares/trpcExpressMiddleware'
 import { appRouter } from 'trpc/routers/index'
 import { createContext } from 'trpc/context'
@@ -9,9 +11,14 @@ dotenv.config()
 const app = express()
 
 app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }))
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use("/trpc", createExpressMiddleware({router: appRouter, createContext}))
 
-app.listen(4000, () => {
-    console.log("Listening on port 4000")
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
 })
