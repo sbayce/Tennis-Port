d64b286b6b98a35e23c6c15ed11e06286ccb80d6
sbayce <youssef2311@gmail.com>
2024-11-19 22:04:09 +0200

    conditionally render product page based on product type

--- apps/client/app/product/[productId]/page.tsx
+++ apps/client/app/product/[productId]/page.tsx
@@ -4,30 +4,34 @@ import PreferenceSelection from "@/components/PreferenceSelection"
 import { Check, XIcon } from "lucide-react"
 import ActionButtons from "@/components/ActionButtons"
 import ProductCarousel from "@/components/ProductCarousel"
+import ShoeSizeSelection from "@/components/ShoeSizeSelection"
 
 const page = async ({params}: { params: Promise<{ productId: string }> }) => {
     const { productId } = await params
-    const racketData = await trpc.getRacket.query(productId)
-    console.log("racket data: ", racketData)
-    if(!racketData) return <h1>Product not found</h1>
+    const productData = await trpc.getProduct.query(productId)
+    console.log("racket data: ", productData)
+    console.log("is shoe: ", productData?.shoe)
+    console.log("is racket: ", productData?.racket)
+    if(!productData) return <h1>Product not found</h1>
   return (
     <div className="flex flex-col lg:flex-row gap-20 py-16">
-        <ProductCarousel racketData={racketData} />
+        <ProductCarousel productData={productData} />
         <div className="flex flex-col gap-2 w-full">
-            <h1 className="text-5xl font-bold">{racketData.name}</h1>
-            <h3 className="text-2xl italic">{racketData.brand}</h3>
-            <p>{racketData.price} <span className="text-sm">EGP</span></p>
+            <h1 className="text-5xl font-bold">{productData.name}</h1>
+            <h3 className="text-2xl italic">{productData.brand}</h3>
+            <p>{productData.price} <span className="text-sm">EGP</span></p>
             <Separator />
-            <PreferenceSelection />
+            {productData.racket && <PreferenceSelection />}
+            {productData.shoe && <ShoeSizeSelection shoeSizes={productData.shoe.size} />}
             <div className="flex gap-2 items-center  mt-4">
-                {racketData.stock > 0? 
+                {productData.stock > 0? 
                     <Check className="h-4 w-4 px-0.5 bg-green-500 text-white rounded-full" strokeWidth={4} /> 
                     : 
                     <XIcon className="h-4 w-4 px-0.5 bg-red-500 text-white rounded-full" strokeWidth={4} />
                 }
-                <p className={racketData.stock > 0? "text-green-500" : "text-red-500"}>{racketData.stock > 0? "In stock" : "Out of stock"}</p>
+                <p className={productData.stock > 0? "text-green-500" : "text-red-500"}>{productData.stock > 0? "In stock" : "Out of stock"}</p>
             </div>
-            <ActionButtons racketData={racketData} />
+            <ActionButtons productData={productData} />
         </div>
     </div>
   )
