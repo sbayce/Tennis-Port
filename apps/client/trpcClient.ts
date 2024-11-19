a100580cabfc6b608f3204aa33e674b9cbda70ad
sbayce <youssef2311@gmail.com>
2024-11-19 03:23:23 +0200

    updated fetch func

--- apps/client/trpcClient.ts
+++ apps/client/trpcClient.ts
@@ -4,12 +4,17 @@ import { AppRouter } from 'trpc/routers/index'
 const trpc = createTRPCProxyClient<AppRouter>({
     links: [httpBatchLink({
         url: String(`${process.env.NEXT_PUBLIC_BACKEND_URL}/trpc`),
-        fetch(url, options) {
-            return fetch(url, {
-                ...options,
-                credentials: 'include'
-            })
-        }
+        headers: () => ({
+            'Content-Type': 'application/json',
+        }),
+        fetch: (input, init) => {
+            console.log('Fetching URL:', input);
+            console.log('Fetch Options:', init);
+            return fetch(input, {
+                ...init,
+                credentials: 'include', // Ensures cookies are sent
+            });
+        },
     })]
 })
 export default trpc
