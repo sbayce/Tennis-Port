9a72898ad0ca5af67c47ba9bf4a907ac6e75c014
sbayce <youssef2311@gmail.com>
2024-11-22 13:11:39 +0200

    global env

--- packages/trpc/trpc.ts
+++ packages/trpc/trpc.ts
@@ -1,8 +1,4 @@
 import { initTRPC } from '@trpc/server'
 import { Context } from './context'
-import dotenv from 'dotenv'
-dotenv.config()
-
-console.log("trBC: ", process.env.STRIPE_SECRET_KEY)
 
 export const t = initTRPC.context<Context>().create()
