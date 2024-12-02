738b1cfb1b49fce8b6aadb8793a1a8fa545042f3
sbayce <youssef2311@gmail.com>
2024-12-02 13:52:22 +0200

    fix

--- packages/trpc/queries/get-available-rackets.ts
+++ packages/trpc/queries/get-available-rackets.ts
@@ -2,59 +2,63 @@ import { t } from "../trpc";
 
 const getAvailableRacketsProcedure = t.procedure.query(async (req) => {
     const { prisma } = req.ctx
-
-    const [typeCountRaw, weightCountRaw, brandCountRaw] = await Promise.all([
-        prisma.racket.groupBy({
-            by: ["type"],
-            _count: { type: true },
-            where: {
-                product: {
+    try{
+        const [typeCountRaw, weightCountRaw, brandCountRaw] = await Promise.all([
+            prisma.racket.groupBy({
+                by: ["type"],
+                _count: { type: true },
+                where: {
+                    product: {
+                        stock: {
+                            gt: 0
+                        }
+                    }
+                },
+                orderBy: { type: "asc" },
+            }),
+            prisma.racket.groupBy({
+                by: ["weight"],
+                _count: { weight: true },
+                where: {
+                    product: {
+                        stock: {
+                            gt: 0
+                        }
+                    }
+                },
+                orderBy: { weight: "asc" },
+            }),
+            prisma.product.groupBy({
+                by: ["brand"],
+                _count: { brand: true },
+                where: { 
+                    category: "RACKET",
                     stock: {
                         gt: 0
                     }
-                }
-            },
-            orderBy: { type: "asc" },
-        }),
-        prisma.racket.groupBy({
-            by: ["weight"],
-            _count: { weight: true },
-            where: {
-                product: {
-                    stock: {
-                        gt: 0
-                    }
-                }
-            },
-            orderBy: { weight: "asc" },
-        }),
-        prisma.product.groupBy({
-            by: ["brand"],
-            _count: { brand: true },
-            where: { 
-                category: "RACKET",
-                stock: {
-                    gt: 0
-                }
-             },
-            orderBy: { brand: "asc" },
-        }),
-    ])
-    // Restructure data for better readability
-    const typesCount = typeCountRaw.map(({ _count, type }) => ({
-        label: type,
-        count: _count.type,
-    }));
-    const weightsCount = weightCountRaw.map(({ _count, weight }) => ({
-        label: weight,
-        count: _count.weight,
-    }));
-    const brandsCount = brandCountRaw.map(({ _count, brand }) => ({
-        label: brand,
-        count: _count.brand,
-    }));
+                },
+                orderBy: { brand: "asc" },
+            }),
+        ])
+        // Restructure data for better readability
+        const typesCount = typeCountRaw.map(({ _count, type }) => ({
+            label: type,
+            count: _count.type,
+        }));
+        const weightsCount = weightCountRaw.map(({ _count, weight }) => ({
+            label: weight,
+            count: _count.weight,
+        }));
+        const brandsCount = brandCountRaw.map(({ _count, brand }) => ({
+            label: brand,
+            count: _count.brand,
+        }));
 
-    return { typesCount, weightsCount, brandsCount }
+        return { typesCount, weightsCount, brandsCount }
+    }catch(error: any) {
+        console.error("Error fetching available rackets:", error.message)
+        throw new Error("Failed to fetch available rackets.")
+    }
 })
 
 export default getAvailableRacketsProcedure
--- packages/trpc/queries/get-available-shoes.ts
+++ packages/trpc/queries/get-available-shoes.ts
@@ -2,82 +2,86 @@ import { t } from "../trpc";
 
 const getAvailableShoesProcedure = t.procedure.query(async (req) => {
     const { prisma } = req.ctx
-
-    const [brandCountRaw, sizeCountRaw, typeCountRaw] = await Promise.all([
-        prisma.product.groupBy({
-            by: ["brand"],
-            _count: {
-                brand: true
-            },
-            where: {
-                category: "SHOE",
-                stock: {
-                    gt: 0
-                }
-            },
-            orderBy: {
-                brand: "asc"
-            }
-        }),
-        prisma.shoe.groupBy({
-            by: ["size"],
-            _count: {
-                size: true
-            },
-            where: {
-                product: {
+    try{
+        const [brandCountRaw, sizeCountRaw, typeCountRaw] = await Promise.all([
+            prisma.product.groupBy({
+                by: ["brand"],
+                _count: {
+                    brand: true
+                },
+                where: {
+                    category: "SHOE",
                     stock: {
                         gt: 0
                     }
+                },
+                orderBy: {
+                    brand: "asc"
                 }
-            },
-            orderBy: {
-                size: "asc"
-            }
-        }),
-        prisma.shoe.groupBy({
-            by: ["type"],
-            _count: {
-                type: true
-            },
-            where: {
-                product: {
-                    stock: {
-                        gt: 0
+            }),
+            prisma.shoe.groupBy({
+                by: ["size"],
+                _count: {
+                    size: true
+                },
+                where: {
+                    product: {
+                        stock: {
+                            gt: 0
+                        }
                     }
+                },
+                orderBy: {
+                    size: "asc"
                 }
-            },
-            orderBy: {
-                type: "asc"
-            }
-        })
-    ])    
-    // Restructure data for better readability
-    const typesCount = typeCountRaw.map(({ _count, type }) => ({
-        label: type,
-        count: _count.type,
-    }));
-    const sizeMap = sizeCountRaw
-        .flatMap((shoe) => shoe.size) // Flatten the array of sizes
-        .reduce((acc, size) => {
-            acc[size] = (acc[size] || 0) + 1; // Count occurrences
-            return acc;
-        }, {} as Record<string, number>);
+            }),
+            prisma.shoe.groupBy({
+                by: ["type"],
+                _count: {
+                    type: true
+                },
+                where: {
+                    product: {
+                        stock: {
+                            gt: 0
+                        }
+                    }
+                },
+                orderBy: {
+                    type: "asc"
+                }
+            })
+        ])    
+        // Restructure data for better readability
+        const typesCount = typeCountRaw.map(({ _count, type }) => ({
+            label: type,
+            count: _count.type,
+        }));
+        const sizeMap = sizeCountRaw
+            .flatMap((shoe) => shoe.size) // Flatten the array of sizes
+            .reduce((acc, size) => {
+                acc[size] = (acc[size] || 0) + 1; // Count occurrences
+                return acc;
+            }, {} as Record<string, number>);
 
-        const sizeCount = Object.entries(sizeMap)
-        .map(([size, count]) => ({ label: size, count }))
-        .sort((a, b) => a.label.localeCompare(b.label)); // Sort sizes
-    console.log("size: ", sizeCountRaw)
-    // const sizeCount = sizeCountRaw.map(({ _count, size }) => ({
-    //     label: size,
-    //     count: _count.size,
-    // }));
-    const brandsCount = brandCountRaw.map(({ _count, brand }) => ({
-        label: brand,
-        count: _count.brand,
-    }));
+            const sizeCount = Object.entries(sizeMap)
+            .map(([size, count]) => ({ label: size, count }))
+            .sort((a, b) => a.label.localeCompare(b.label)); // Sort sizes
+        console.log("size: ", sizeCountRaw)
+        // const sizeCount = sizeCountRaw.map(({ _count, size }) => ({
+        //     label: size,
+        //     count: _count.size,
+        // }));
+        const brandsCount = brandCountRaw.map(({ _count, brand }) => ({
+            label: brand,
+            count: _count.brand,
+        }));
 
-    return { typesCount, sizeCount, brandsCount }
+        return { typesCount, sizeCount, brandsCount }
+    }catch(error: any) {
+        console.error("Error fetching available shoes:", error.message)
+        throw new Error("Failed to fetch available shoes.")
+    }
 })
 
 export default getAvailableShoesProcedure
