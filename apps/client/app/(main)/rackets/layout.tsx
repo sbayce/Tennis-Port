3810ee406201ef5b7bece9b00590840459fc1ccf
sbayce <youssef2311@gmail.com>
2024-12-02 14:37:23 +0200

    test

--- apps/client/app/(main)/rackets/layout.tsx
+++ apps/client/app/(main)/rackets/layout.tsx
@@ -1,7 +1,7 @@
 import MainDisplay from "@/components/MainDisplay"
 import SideBar from "@/components/SideBar"
 import ActiveFilters from "@/components/ActiveFilters"
-import trpc from "@/trpcClient"
+// import trpc from "@/trpcClient"
 import SortMenu from "@/components/SortMenu"
 
 export default async function RootLayout({
@@ -9,16 +9,16 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
-    const { brandsCount, typesCount, weightsCount } = await trpc.getAvailableRackets.query()
-    console.log(brandsCount)
-    console.log(typesCount)
-    console.log(weightsCount)
-    const sidebarData = {type: typesCount, brand: brandsCount, weight: weightsCount}
+    // const { brandsCount, typesCount, weightsCount } = await trpc.getAvailableRackets.query()
+    // console.log(brandsCount)
+    // console.log(typesCount)
+    // console.log(weightsCount)
+    // const sidebarData = {type: typesCount, brand: brandsCount, weight: weightsCount}
   return (
     <>
       <MainDisplay />
       <div className="flex gap-2">
-        <SideBar data={sidebarData} />
+        <SideBar data={{type: [], brand: []}} />
         <div className="flex flex-col gap-8 mb-20 w-full">
           <div className="flex justify-between mt-4">
             <ActiveFilters />
